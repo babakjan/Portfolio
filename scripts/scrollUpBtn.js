abfdeb77e32e850f7bcaa31dd8e45eb302bad78b
Jan Babák <babakjan@fit.cvut.cz>
2023-03-12 20:46:09 +0100

    Add: Administration of e-shop site

--- scripts/scrollUpBtn.js
+++ scripts/scrollUpBtn.js
@@ -7,7 +7,7 @@ window.addEventListener("resize", setScrollUpBtnPosition);
 
 function setScrollUpBtnPosition() {
 	scrollUpBtn.style.right = `calc(${window.innerWidth - main.getBoundingClientRect().right}px + ${
-		window.innerWidth > 570 ? 2 : 1
+		window.innerWidth > 570 ? 1 : 0
 	}rem)`;
 }
 
